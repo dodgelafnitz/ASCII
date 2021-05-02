e319549bebbe984ae22a8b92ae50a5dbbfae4354
dodgelafnitz <dodgelafnitz@gmail.com>
2021-05-02 13:14:43 -0700

    Added comments on windows console functions/macros needed.

--- ASCII/projects/Ascii/source/Window/Window.cpp
+++ ASCII/projects/Ascii/source/Window/Window.cpp
@@ -4,4 +4,52 @@
 
 #include "Window/Window.h"
 
+/*
 
+Make Cursor Invisile:
+  CONSOLE_CURSOR_INFO
+  SetConsoleCursor()
+
+Enable Mouse
+  ENABLE_MOUSE_INPUT
+  SetConsoleModw()
+
+Set Console Properties
+  WS_MAXIMIZEBOX
+  WS_SIZEBOX
+  GWL_STYLE
+  SetWindowLong()
+
+Get Time
+  QueryPerformanceCounter()
+  QueryPerformanceFrequency()
+
+Set Font
+  CONSOLE_FONT_INFOEX
+  L"Terminal"
+  FF_DONTCARE
+  FW_NORMAL
+  SetCurrentConsoleFontEx()
+
+Draw
+  WriteConsoleOutput()
+
+Get Input
+  GetNumberOfConsoleInputEvents()
+  ReadConsoleInput()
+
+Get or Set Clipboard
+  OpenClipboard()
+  CF_TEXT
+  GetClipboardData()
+  GMEM_MOVABLE
+  GlobalAlloc()
+  SetClipboardData()
+  GlobalLock()
+  GlobalUnlock()
+  CloseClipboard
+
+Set Title
+  SetConsoleTitle()
+
+*/
