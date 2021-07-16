f58abe82bae6a63eba102e0ea372cbbca33bd228
dodgelafnitz <dodgelafnitz@gmail.com>
2021-07-15 23:31:53 -0700

    Finished Ray Tests.

--- ASCII/projects/DcUtility/source/Math/Shapes2D/Ray.cpp
+++ ASCII/projects/DcUtility/source/Math/Shapes2D/Ray.cpp
@@ -58,3 +58,40 @@ LineSegment Ray::GetLineSegment(float length) const {
   return LineSegment(m_root, m_root + length * m_dir);
 }
 
+LineSegment Ray::GetLineSegment(float length, fvec2 const & facing) const {
+  LineSegment result = GetLineSegment(length);
+
+  result.FaceTowards(facing);
+
+  return result;
+}
+
+float Ray::DistanceTo(fvec2 const & point) const {
+  fvec2 const diff = point - m_root;
+
+  if (diff.Dot(m_dir) < 0.0f) {
+    return diff.Length();
+  }
+  else {
+    return std::abs(diff.Dot(GetNormal()));
+  }
+}
+
+fvec2 Ray::ProjectPointOntoRay(fvec2 const & point) const {
+  fvec2 const diff = point - m_root;
+  float const dist = diff.Dot(m_dir);
+
+  if (dist < 0.0f) {
+    return m_root;
+  }
+  else {
+    return dist * m_dir + m_root;
+  }
+}
+fvec2 Ray::ProjectPointOntoLine(fvec2 const & point) const {
+  fvec2 const diff = point - m_root;
+  float const dist = diff.Dot(m_dir);
+
+  return dist * m_dir + m_root;
+}
+
--- ASCII/projects/DcUtility/test/Math/Shapes2D/RayTest.cpp
+++ ASCII/projects/DcUtility/test/Math/Shapes2D/RayTest.cpp
@@ -169,50 +169,142 @@ TEST(RayTest, AnyRay_ObtainedNegativeLengthLineSegmentFromRay_RootIsP0OrP1) {
 }
 
 TEST(RayTest, AnyRay_ObtainedLineSegmentFromRayWithFacing_LengthIsCorrect) {
-  FAIL();
+  Ray const ray(fvec2(-2.4f, 2.7f), fvec2(2.2f, -1.0f));
+
+  LineSegment const seg = ray.GetLineSegment(12.0f, fvec2(-3.0f, -12.4f));
+
+  EXPECT_FLOAT_EQ(seg.Length(), 12.0f);
 }
 
 TEST(RayTest, AnyRay_ObtainedLineSegmentFromRayWithFacing_RootIsP0OrP1) {
-  FAIL();
+  fvec2 const root(3.4f, 20.8f);
+  Ray const   ray(root, fvec2(0.1f, 0.8f));
+
+  LineSegment const seg = ray.GetLineSegment(0.4f, fvec2(3.0f, 1.3f));
+
+  bool const rootIsEndpoint = root == seg.p0 || root == seg.p1;
+
+  EXPECT_TRUE(rootIsEndpoint) <<
+    "  root:   {" << root.x << ", " << root.y << "}" << std::endl <<
+    "  seg.p0: {" << seg.p0.x << ", " << seg.p0.y << "}" << std::endl <<
+    "  seg.p1: {" << seg.p1.x << ", " << seg.p1.y << "}" << std::endl
+  ;
 }
 
 TEST(RayTest, AnyRay_ObtainedLineSegmentFromRayWithFacing_NormalFacesTarget) {
-  FAIL();
+  fvec2 const root(14.3f, -2.4f);
+  Ray const   ray(root, fvec2(-0.8f, -0.6f));
+  fvec2 const target(0.3f, -5.4f);
+
+  LineSegment const seg = ray.GetLineSegment(3.1f, target);
+
+  fvec2 const diff = target - root;
+
+  EXPECT_GT(diff.Dot(seg.GetNormal()), 0.0f);
 }
 
 TEST(RayTest, AnyRay_ObtainedNegativeLengthLineSegmentFromRayWithFacing_LengthIsCorrect) {
-  FAIL();
+  fvec2 const root(0.4f, 18.2f);
+  Ray const   ray(root, fvec2(0.5f, 0.4f));
+  fvec2 const target(0.7f, 2.3f);
+
+  LineSegment const seg = ray.GetLineSegment(-8.0f, target);
+
+  EXPECT_FLOAT_EQ(seg.Length(), 8.0f);
 }
 
 TEST(RayTest, AnyRay_ObtainedNegativeLengthLineSegmentFromRayWithFacing_SegmentGoesInCorrectDirection) {
-  FAIL();
+  fvec2 const root(-3.7f, -4.2f);
+  Ray const   ray(root, fvec2(1.0f, 0.0f));
+  fvec2 const target(-0.4f, 6.7f);
+
+  LineSegment const seg = ray.GetLineSegment(-4.2f, target);
+
+  fvec2 const diff = (seg.p1 - root) + (seg.p0 - root);
+
+  EXPECT_EQ(diff.Normal(), -ray.GetDirection());
 }
 
 TEST(RayTest, AnyRay_ObtainedNegativeLengthLineSegmentFromRayWithFacing_NormalFacesTarget) {
-  FAIL();
+  fvec2 const root(-2.5f, 12.2f);
+  Ray const   ray(root, fvec2(-0.1f, -0.8f));
+  fvec2 const target(10.2f, 0.1f);
+
+  LineSegment const seg = ray.GetLineSegment(-8.2f, target);
+
+  fvec2 const diff = target - root;
+
+  EXPECT_GT(diff.Dot(seg.GetNormal()), 0.0f);
 }
 
 TEST(RayTest, AnyRay_CheckDistanceToPointAlongRayPath_DistanceIsCorrect) {
-  FAIL();
+  Ray const   ray(fvec2(1.0f, 0.0f), fvec2(-0.6f, -0.8f));
+  fvec2 const target(1.0f, -3.0f);
+
+  EXPECT_FLOAT_EQ(ray.DistanceTo(target), 1.8f);
 }
 
 TEST(RayTest, AnyRay_CheckDistanceToPointBeforeRayPath_DistanceIsCorrect) {
-  FAIL();
+  Ray const   ray(fvec2(2.0f, -4.0f), fvec2(0.8f, -0.6f));
+  fvec2 const target(-2.0f, -1.0f);
+
+  EXPECT_FLOAT_EQ(ray.DistanceTo(target), 5.0f);
 }
 
 TEST(RayTest, AnyRay_ProjectPointOntoRayBeforeRayPath_ProjectionIsRoot) {
-  FAIL();
+  Ray const   ray(fvec2(1.2f, 2.7f), fvec2(-0.2f, -0.1f));
+  fvec2 const target(1.7f, 4.9f);
+
+  EXPECT_EQ(ray.ProjectPointOntoRay(target), ray.GetRoot());
 }
 
 TEST(RayTest, AnyRay_ProjectPointOntoRayAlongRayPath_ProjectionIsClosestPointToPoint) {
-  FAIL();
+  Ray const   ray(fvec2(1.5f,-4.5f), fvec2(1.0f, 1.0f));
+  fvec2 const target(4.0f, 8.0f);
+
+  EXPECT_EQ(ray.ProjectPointOntoRay(target), fvec2(9.0f, 3.0f));
 }
 
-TEST(RayTest, AnyRay_ProjectPointOntoLineBeforeRayPath_ProjectionIsAlnogDirectionFromRoot) {
-  FAIL();
+TEST(RayTest, AnyRay_ProjectPointOntoLineBeforeRayPath_ProjectionIsAlongDirectionFromRoot) {
+  Ray const   ray(fvec2(5.0f, 2.0f), fvec2(0.6f, -0.8f));
+  fvec2 const target(2.0f, 8.0f);
+
+  fvec2 const projected = ray.ProjectPointOntoLine(target);
+  fvec2 const diff      = ray.GetRoot() - projected;
+  float const dist      = diff.Length() * (diff.Dot(ray.GetDirection()) > 0.0f ? 1.0f : -1.0f);
+
+  EXPECT_EQ(projected + dist * ray.GetDirection(), ray.GetRoot());
 }
 
-TEST(RayTest, AnyRay_ProjectPointOntoLineAlongRayPath_ProjectionIsAlnogDirectionFromRoot) {
-  FAIL();
+TEST(RayTest, AnyRay_ProjectPointOntoLineAlongRayPath_ProjectionIsAlongDirectionFromRoot) {
+  Ray const   ray(fvec2(-2.7f, -8.4f), fvec2(-0.1f, 0.9f));
+  fvec2 const target(-5.3f, 9.2f);
+
+  fvec2 const projected = ray.ProjectPointOntoLine(target);
+  fvec2 const diff = ray.GetRoot() - projected;
+  float const dist = diff.Length() * (diff.Dot(ray.GetDirection()) > 0.0f ? 1.0f : -1.0f);
+
+  fvec2 const expectedRoot = projected + dist * ray.GetDirection();
+
+  EXPECT_FLOAT_EQ(expectedRoot.x, ray.GetRoot().x);
+  EXPECT_FLOAT_EQ(expectedRoot.y, ray.GetRoot().y);
+}
+
+TEST(RayTest, AnyRay_ProjectPointOntoLineBeforeRayPath_ProjectionIsClosestPointOnLineToTarget) {
+  Ray const   ray(fvec2(0.3f, 15.3f), fvec2(-0.5f, 0.2f));
+  fvec2 const target(12.6f, 18.0f);
+
+  fvec2 const projected = ray.ProjectPointOntoLine(target);
+
+  EXPECT_FLOAT_EQ((target - projected).Length(), ray.GetLine().DistanceTo(target));
+}
+
+TEST(RayTest, AnyRay_ProjectPointOntoLineAlongRayPath_ProjectionIsClosestPointOnLineToTarget) {
+  Ray const   ray(fvec2(-1.4f, -2.5), fvec2(1.2f, 0.3f));
+  fvec2 const target(9.2f, 6.7f);
+
+  fvec2 const projected = ray.ProjectPointOntoLine(target);
+
+  EXPECT_FLOAT_EQ((target - projected).Length(), ray.GetLine().DistanceTo(target));
 }
 
