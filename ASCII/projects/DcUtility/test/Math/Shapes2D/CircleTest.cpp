06147afedef070dde0c440754b35e8177e53c076
dodgelafnitz <dodgelafnitz@gmail.com>
2021-07-28 22:35:15 -0700

    Started adding Circle test implementations.

--- ASCII/projects/DcUtility/test/Math/Shapes2D/CircleTest.cpp
+++ ASCII/projects/DcUtility/test/Math/Shapes2D/CircleTest.cpp
@@ -6,59 +6,113 @@
 #include "gtest/gtest.h"
 
 TEST(CircleTest, DefaultConstructed_CheckRadius_RadiusIsNonNegative) {
-  FAIL();
+  Circle const circle;
+
+  EXPECT_GE(circle.GetRadius(), 0.0f);
+}
+
+TEST(CircleTest, DefaultConstructed_CheckPosition_PositionIs0) {
+  Circle const circle;
+
+  EXPECT_EQ(circle.GetPosition(), fvec2());
 }
 
 TEST(CircleTest, ConstructedWithValues_CheckPosition_PositionIsCorrect) {
-  FAIL();
+  fvec2 const  pos(1.4f, 2.3f);
+  Circle const circle(pos, 9.2f);
+
+  EXPECT_EQ(circle.GetPosition(), pos);
 }
 
 TEST(CircleTest, ConstructedWithValues_CheckRadius_RadiusIsCorrect) {
-  FAIL();
+  float const  radius = 3.8f;
+  Circle const circle(fvec2(-1.8f, 0.3f), radius);
+
+  EXPECT_EQ(circle.GetRadius(), radius);
 }
 
 TEST(CircleTest, ConstructedWithNegativeRadius_CheckRadius_RadiusIsNonNegative) {
-  FAIL();
+  float const  radius = -4.9f;
+  Circle const circle(fvec2(10.4f, -4.7f), radius);
+
+  EXPECT_EQ(circle.GetRadius(), -radius);
 }
 
 TEST(CircleTest, AnyCircle_PositionIsSet_PositionIsCorrect) {
-  FAIL();
+  fvec2 const pos(9.3f, 5.4f);
+  Circle      circle(fvec2(1.4f, -0.5f), 10.2f);
+
+  circle.SetPosition(pos);
+
+  EXPECT_EQ(circle.GetPosition(), pos);
 }
 
 TEST(CircleTest, AnyCircle_RadiusIsSetToPositive_RadiusIsCorrect) {
-  FAIL();
+  float const radius = 2.2f;
+  Circle      circle(fvec2(-4.0f, 6.3f), 2.4f);
+
+  circle.SetRadius(radius);
+
+  EXPECT_EQ(circle.GetRadius(), radius);
 }
 
 TEST(CircleTest, AnyCircle_RadiusIsSetToNegative_RadiusIsPositive) {
-  FAIL();
+  float const radius = -5.9f;
+  Circle      circle(fvec2(1.0f, 1.0f), 0.2f);
+
+  circle.SetRadius(radius);
+
+  EXPECT_EQ(circle.GetRadius(), -radius);
 }
 
 TEST(CircleTest, AnyCircle_RadiusIsSetTo0_RadiusIs0) {
-  FAIL();
+  Circle circle(fvec2(-12.7f, -2.5f), 3.4f);
+
+  circle.SetRadius(0.0f);
+
+  EXPECT_EQ(circle.GetRadius(), 0.0f);
 }
 
 TEST(CircleTest, AnyCircle_CheckContainsContainedPoint_PointIsContained) {
-  FAIL();
+  Circle const circle(fvec2(0.4f, 2.1f), 8.8f);
+  fvec2 const  target(1.3f, 0.4f);
+
+  EXPECT_TRUE(circle.Contains(target));
 }
 
 TEST(CircleTest, AnyCircle_CheckContainsExternalPoint_PointIsNotContained) {
-  FAIL();
+  Circle const circle(fvec2(-2.6f, 1.3f), 4.2f);
+  fvec2 const  target(-4.1f, -6.2f);
+
+  EXPECT_FALSE(circle.Contains(target));
 }
 
 TEST(CircleTest, AnyCircle_CheckContainsBoundaryPoint_PointIsContained) {
-  FAIL();
+  Circle const circle(fvec2(0.3f, 0.6f), 1.0f);
+  fvec2 const  target(0.3f, 1.6f);
+
+  EXPECT_TRUE(circle.Contains(target));
 }
 
 TEST(CircleTest, AnyCircle_CheckDistanceToContainedPoint_DistanceIs0) {
-  FAIL();
+  Circle const circle(fvec2(1.7f, -4.2f), 4.2f);
+  fvec2 const  target(1.2f, -2.1f);
+
+  EXPECT_EQ(circle.DistanceTo(target), 0.0f);
 }
 
 TEST(CircleTest, AnyCircle_CheckDistanceToExternalPoint_DistanceIsCorrect) {
-  FAIL();
+  Circle const circle(fvec2(0.2f, 0.5f), 2.3f);
+  fvec2 const  target(0.2f, -5.3f);
+
+  EXPECT_FLOAT_EQ(circle.DistanceTo(target), 3.5f);
 }
 
 TEST(CircleTest, AnyCircle_CheckDistanceToBoundaryPoint_DistanceIs0) {
-  FAIL();
+  Circle const circle(fvec2(7.3f, -2.4f), 10.2f);
+  fvec2 const  target(7.3f, 7.8f);
+
+  EXPECT_FLOAT_EQ(circle.DistanceTo(target), 0.0f);
 }
 
 TEST(CircleTest, AnyCircle_ClampContainedPoint_ResultIsTarget) {
