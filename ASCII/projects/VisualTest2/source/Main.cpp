3aa2831a00b083081df26353804f59d5789df41b
dodgelafnitz <dodgelafnitz@gmail.com>
2022-05-13 15:57:24 -0700

    Initial glfw stuff and testing.

--- ASCII/projects/VisualTest2/source/Main.cpp
+++ ASCII/projects/VisualTest2/source/Main.cpp
@@ -2,7 +2,21 @@
  * Copywrite 2022 Dodge Lafnitzegger
  */
 
-int main(int argc, char const ** argv) {
+#include "GLFW/glfw3.h"
+
+int main(void) {
+  if (!glfwInit()) {
+    return 1;
+  }
+
+  GLFWwindow * window0 = glfwCreateWindow(640, 360, glfwGetMonitorName(glfwGetPrimaryMonitor()), nullptr, nullptr);
+  GLFWwindow * window1 = glfwCreateWindow(640, 360, "Test", nullptr, nullptr);
+
+  while (true) {
+    glfwWaitEvents();
+  }
+
+  glfwTerminate();
   return 0;
 }
 
